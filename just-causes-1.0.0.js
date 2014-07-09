88ae1d18aca8e50aa1b986d8dfeaae49ae00f642
Alwyn Lombaard <alwyn.lombaard@yahoo.co.uk>
2014-07-09 09:21:42 +0100

    Update just-causes-1.0.0.js

--- just-causes-1.0.0.js
+++ just-causes-1.0.0.js
@@ -13,10 +13,14 @@
 		  return -1;
 		};
 	}
-	if(!global.JustCauses) { global.JustCauses = {}; };
+	if(!global.JustCauses) { 
+		global.JustCauses = {}; 
+	}
 	var JustCauses = global.JustCauses;
 	
-	if(!JustCauses.processedScripts) { JustCauses.processedScripts = []; };
+	if(!JustCauses.processedScripts) { 
+		JustCauses.processedScripts = []; 
+	}
 	var processedScripts = JustCauses.processedScripts;
 	
 	if(!JustCauses.styleTags) { JustCauses.styleTags = []; };
@@ -26,28 +30,28 @@
 	var thisRequestUrl = 'https://raw.githubusercontent.com/alwynlombaard/harmonograph/gh-pages/just-causes-1.0.0.js';
 	
 	for(var i = 0; i < scriptTags.length; i++) {
-    var scriptTag = scriptTags[i];
+		var scriptTag = scriptTags[i];
 	
-	if (scriptTag.src === thisRequestUrl && processedScripts.indexOf(scriptTag) < 0) {
-		processedScripts.push(scriptTag);
-		if(styleTags.length === 0) {
-			var styleTag = document.createElement("link");
-			styleTag.rel = "stylesheet";
-			styleTag.type = "text/css";
-			styleTag.href =  "https://raw.githubusercontent.com/alwynlombaard/harmonograph/gh-pages/just-causes-1.0.0.css";
-			styleTag.media = "all";
-			document.getElementsByTagName('head')[0].appendChild(styleTag);
-			styleTags.push(styleTag);
-      }
-	  
-	  var div = document.createElement('div');
-      div.id = 'just-causes-id';
-	  div.className = 'just-causes';
-	  
-	  scriptTag.parentNode.insertBefore(div, scriptTag);
-	  
-	  div.innerHTML = 'hello from just causes';
+		if (scriptTag.src === thisRequestUrl && processedScripts.indexOf(scriptTag) < 0) {
+			processedScripts.push(scriptTag);
+			if(styleTags.length === 0) {
+				var styleTag = document.createElement("link");
+				styleTag.rel = "stylesheet";
+				styleTag.type = "text/css";
+				styleTag.href =  "https://raw.githubusercontent.com/alwynlombaard/harmonograph/gh-pages/just-causes-1.0.0.css";
+				styleTag.media = "all";
+				document.getElementsByTagName('head')[0].appendChild(styleTag);
+				styleTags.push(styleTag);
+		  }
+		  
+		  var div = document.createElement('div');
+		  div.id = 'just-causes-id';
+		  div.className = 'just-causes';
+		  
+		  scriptTag.parentNode.insertBefore(div, scriptTag);
+		  
+		  div.innerHTML = 'hello from just causes';
 
+		}
 	}
-	
-}(this);
+})(this);
