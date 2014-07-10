a0d123a83dfa1cd60407fcd75d8dde69b33bb104
Alwyn Lombaard <alwyn.lombaard@yahoo.co.uk>
2014-07-10 09:49:57 +0100

    Update just-causes-1.0.0.js

--- just-causes-1.0.0.js
+++ just-causes-1.0.0.js
@@ -56,7 +56,17 @@
 		  scriptTag.parentNode.insertBefore(div, scriptTag);
 		  
 		  div.innerHTML = 'hello from just causes';
-
+		  
+		   $.ajax({
+                url: "https://api.justgiving.com/4066ece8/v1/account/alwyn@justgiving.com/pages",
+                method: "GET",
+				contentType: "application/json",
+				dataType: 'jsonp'
+            }).done(function(data) {
+				console.log(data);
+            }).always(function()
+				console.log("always");
+			});
 		}
 	}
 })(this);
